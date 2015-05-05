b6626e4b5fc9609b457a7eada82df11f8a2980c2
andreasbundgaard <andreasb94@live.dk>
2015-05-05 09:58:09 +0200

    Hej

---
+++ PDF_Test/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using MigraDoc.RtfRendering;
+
+namespace PDF_Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    ///Hej 
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string line = null;
+                TextReader readFile = new StreamReader("Text.txt");
+                int yPoint = 0;
+
+                PdfDocument pdf = new PdfDocument();
+                pdf.Info.Title = "TXT to PDF";
+                PdfPage pdfPage = pdf.AddPage();
+                XGraphics graph = XGraphics.FromPdfPage(pdfPage);
+                XFont font = new XFont("Courier New", 11, XFontStyle.Regular);
+
+                while (true)
+                {
+                    line = readFile.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        graph.DrawString(line, font, XBrushes.Black, new XRect(40, yPoint, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.TopLeft);
+                        yPoint = yPoint + 10;
+                    }
+                }
+                
+                    string pdfFilename = "txttopdf.pdf";
+                    pdf.Save(pdfFilename);
+                    readFile.Close();
+                    readFile = null;
+                    Process.Start(pdfFilename);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+}
