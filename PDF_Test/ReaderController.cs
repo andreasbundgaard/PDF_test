17820fac907d1e2921d3d8b3b036dd053dd7a8ba
JonasOlesen <jonaswolesen@hotmail.com>
2015-05-14 10:39:44 +0200

    Firmanavn bliver trimmet

--- PDF_Test/ReaderController.cs
+++ PDF_Test/ReaderController.cs
@@ -32,24 +32,8 @@ namespace PDF_Test
         {
             string output;
             output = input.Substring(7, 20);
-            output.Replace("  ", string.Empty);
-            return output;
+            return output.Trim();
         }
 
-        private static StringBuilder TrimEnd(this StringBuilder sb)
-        {
-            if (sb == null || sb.Length == 0)
-                return sb;
-
-            int i = sb.Length - 1;
-            for (; i >= 0; i--)
-                if (!char.IsWhiteSpace(sb[i]))
-                    break;
-
-            if (i < sb.Length - 1)
-                sb.Length = i + 1;
-
-            return sb;
-        }
     }
 }
