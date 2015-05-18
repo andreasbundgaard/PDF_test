519033f3be8e7a33a0be240e169588246d0b3cd8
JonasOlesen <jonaswolesen@hotmail.com>
2015-05-18 10:58:55 +0200

    PdfController oprettet

---
+++ PDF_Test/PdfController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF_Test
+{
+    class PdfController
+    {
+        
+        
+        public void CreateInvoice(Invoice input)
+        {
+            foreach (List<string> LineList in input.Pages)
+            {
+
+            }
+        }
+    }
+}
